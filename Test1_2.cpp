1b5dcd16319435eef77d6b009b8a3ecc97d371dd
seominb <zyuqw1193@naver.com>
2021-05-02 06:54:06 -0700

    test1_2.cpp 수정 및 주석 추가

--- Test1_2.cpp
+++ Test1_2.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
-void printAllKLengthRec(char *set, string prefix, int n, int k);
+void permutations(char *array, string prefix, int n, int k);
 int main(void)
 {
     char array[2] = {'a', 'b'};
     int n;
     cin>>n;
-    printAllKLengthRec(array,"",2, n);
+    permutations(array,"",2, n);
      
 }
-void printAllKLengthRec(char set[], string prefix, int n, int k)
+void permutations(char *array, string prefix, int n, int k)
 {
     if (k == 0)
     {
@@ -19,8 +19,10 @@ void printAllKLengthRec(char set[], string prefix, int n, int k)
     for (int i = 0; i < n; i++)
     {
         string newPrefix;
-        newPrefix = prefix + set[i];
-        printAllKLengthRec(set, newPrefix, n, k - 1);
+        newPrefix = prefix + array[i];
+        permutations(array, newPrefix, n, k - 1);
     }
  
 }
+//https://www.geeksforgeeks.org/print-all-combinations-of-given-length/
+//위 사이트를 참고하였습니다.
